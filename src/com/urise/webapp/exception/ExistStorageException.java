6a617f8a72d17643726485b7754038e5e5a6008e
se4oev <p.karpenko26@gmail.com>
2023-08-14 17:53:25 +0300

    [!] добавились исключения;

--- src/com/urise/webapp/exception/ExistStorageException.java
+++ src/com/urise/webapp/exception/ExistStorageException.java
@@ -7,7 +7,7 @@ package com.urise.webapp.exception;
 public class ExistStorageException extends StorageException {
 
     public ExistStorageException(String uuid) {
-        super("Resume already exist", uuid);
+        super("Resume " + uuid + " already exist", uuid);
     }
 
 }
--- src/com/urise/webapp/exception/NotExistStorageException.java
+++ src/com/urise/webapp/exception/NotExistStorageException.java
@@ -7,7 +7,7 @@ package com.urise.webapp.exception;
 public class NotExistStorageException extends StorageException {
 
     public NotExistStorageException(String uuid) {
-        super("Resume not exist", uuid);
+        super("Resume " + uuid + " not exist", uuid);
     }
 
 }
