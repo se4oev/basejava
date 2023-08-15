d34fa3164c55568c840d51528bb7eed5dbcb4a76
se4oev <p.karpenko26@gmail.com>
2023-08-15 11:10:51 +0300

    [-] убрал тесты для непубличных методов;

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -1,46 +1,9 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-import org.junit.Assert;
-import org.junit.Test;
-
 public class ArrayStorageTest extends AbstractArrayStorageTest {
 
-    private static final ArrayStorage storage = new ArrayStorage();
-
     public ArrayStorageTest() {
-        super(storage);
-    }
-
-    @Test
-    public void getIndex() {
-        int index1 = storage.getIndex(UUID_1);
-        Assert.assertEquals(0, index1);
-
-        int index2 = storage.getIndex(UUID_2);
-        Assert.assertEquals(1, index2);
-
-        int index3 = storage.getIndex(UUID_3);
-        Assert.assertEquals(2, index3);
-    }
-
-    @Test
-    public void insertElement() {
-        Resume resume = new Resume("dummy");
-        storage.insertElement(resume, 3);
-
-        Assert.assertEquals(3, storage.size());
-        storage.size++;
-
-        Resume fromStorage = storage.get("dummy");
-        Assert.assertNotNull(fromStorage);
-    }
-
-    @Test
-    public void fillDeletedElement() {
-        storage.fillDeletedElement(1);
-        int index = storage.getIndex(UUID_3);
-        Assert.assertEquals(1, index);
+        super(new ArrayStorage());
     }
 
 }
--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -1,23 +1,9 @@
 package com.urise.webapp.storage;
 
-import org.junit.Test;
-
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
 
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
-    }
-
-    @Test
-    public void insertElement() {
-    }
-
-    @Test
-    public void fillDeletedElement() {
-    }
-
-    @Test
-    public void getIndex() {
     }
 
 }
