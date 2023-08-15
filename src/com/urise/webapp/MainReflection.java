d412ffd8f60189f46f2c25b4d62a45291d226e43
se4oev <p.karpenko26@gmail.com>
2023-08-15 11:12:47 +0300

    [!] вызвал метод toString через рефлексию;

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,6 +3,8 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by karpenko on 14.08.2023.
@@ -10,7 +12,7 @@ import java.lang.reflect.Field;
  */
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -22,6 +24,10 @@ public class MainReflection {
         field.set(resume, "5553535");
         Object o1 = field.get(resume);
         System.out.println(o1);
+
+        Method toString = resume.getClass().getDeclaredMethod("toString");
+        String str = (String) toString.invoke(resume);
+        System.out.println(str);
     }
 
 }
