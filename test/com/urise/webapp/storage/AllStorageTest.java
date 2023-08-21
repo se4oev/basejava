86d6568848458b7201b73e2bd542b1c0b5ea6412
se4oev <p.karpenko26@gmail.com>
2023-08-21 17:28:40 +0300

    [!] HW6 тестирование с Suite;

---
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -0,0 +1,18 @@
+package com.urise.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by karpenko on 21.08.2023.
+ * Description:
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapStorageTest.class
+})
+public class AllStorageTest {
+}
